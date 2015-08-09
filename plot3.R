43220d4d9b9c738fba9702770137c3712cd42921
MichaelSzczepaniak <mszczepaniak@pobox.com>
2015-08-09 13:16:56 -0600

    fix plot3 rendering issue, revise axis labeling

--- plot3.R
+++ plot3.R
@@ -1,20 +1,23 @@
+##
+## For some strange reason this code runs fine if it run using:
+##         source("plot3.R", echo = TRUE, print.eval = TRUE)
+## However, if it is run using just:
+##         source("plot3.R")
+## it generates a blank png file on my system that is the specified size
+## (720 x 480 pixels)
+##
 library(dplyr)
 library(ggplot2)
-##
 NEI <- readRDS("summarySCC_PM25.rds")
 
 neiBaltimore <- filter(NEI, fips == "24510")
 neiBaltimoreByYearByType <- group_by(neiBaltimore, year, type)
 totalEmissions <- summarise(neiBaltimoreByYearByType,
                             TotalEmissions = sum(Emissions, na.rm = TRUE))
-totEmPoint <- filter(totalEmissions, type == "POINT")
-totEmNonPoint <- filter(totalEmissions, type == "NONPOINT")
-totEmOnRoad <- filter(totalEmissions, type == "ON-ROAD")
-totEmNonRoad <- filter(totalEmissions, type == "NON-ROAD")
 png(file = "plot3.png", width = 720, height = 480, units = "px")
-qplot(year, TotalEmissions, data = totalEmissions,
-      main = "Baltimore City PM2.5 Emissions By Year and Type",
-      xlab = "Year",
-      ylab = "Emissions (tons)",
-      color = type, geom = c("point", "line"))
+plot <- qplot(year, TotalEmissions, data = totalEmissions,
+        main = "Baltimore City PM2.5 Emissions By Year and Type",
+        color = type, geom = c("point", "line"),
+        xlab = "Year", ylab = "PM 2.5 Emissions (tons)")
+print(plot)
 dev.off()
